8813b6e8f3c64daad78bfc77f52e08a80f21ad66
frooody <mister.dennisk@gmail.com>
2023-07-17 21:06:17 +0500

    Configure registration page

--- register/views.py
+++ register/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+# Create your iews here.
 
 def index(request):
-    return render(request, 'register/register.html')
+    form = UserCreationForm()
+    context = {"form": form}
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'register/register.html', context)
