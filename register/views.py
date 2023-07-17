e80c1ea6ce684bfd02e72587b9de491bc9af638f
frooody <mister.dennisk@gmail.com>
2023-07-18 01:42:23 +0500

    Modified

--- register/views.py
+++ register/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 # Create your iews here.
+from .forms import NewUserForm
 
 def index(request):
-    form = UserCreationForm()
+    form = NewUserForm()
     context = {"form": form}
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = NewUserForm(request.POST)
         if form.is_valid():
             form.save()
     return render(request, 'register/register.html', context)
